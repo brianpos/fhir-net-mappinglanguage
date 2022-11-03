4a13a97c49ee58d1777e668ccb681ffa3fc3fdf3
Brian Postlethwaite <brian_pos@hotmail.com>
2022-11-03 22:03:39 +1100

    Implement the other service calls required for the exexution engine

--- demo-map-server/Services/StructureMapService.cs
+++ demo-map-server/Services/StructureMapService.cs
@@ -198,7 +198,7 @@ namespace demo_map_server.Services
                         return false;
                     });
 
-                var mapServices = new InlineServices(outcome);
+                var mapServices = new InlineServices(outcome, provider);
                 var engine = new StructureMapUtilitiesExecute(worker, mapServices, provider);
                 var target = engine.GenerateEmptyTargetOutputStructure(sm);
                 engine.transform(null, resource.ToTypedElement(), sm, target);
@@ -273,19 +273,21 @@ namespace demo_map_server.Services
         private class InlineServices : StructureMapUtilitiesAnalyze.ITransformerServices
         {
             public List<KeyValuePair<string, string>> LogMessages { get; private set; } = new List<KeyValuePair<string, string>>();
-            internal InlineServices(OperationOutcome outcome)
+            internal InlineServices(OperationOutcome outcome, IStructureDefinitionSummaryProvider provider)
             {
                 _outcome = outcome;
+                _provider = provider;
             }
             private OperationOutcome _outcome;
+            private IStructureDefinitionSummaryProvider _provider;
             public ITypedElement createResource(object appInfo, ITypedElement res, bool atRootofTransform)
             {
-                throw new NotImplementedException();
+                return res;
             }
 
             public ITypedElement createType(object appInfo, string name)
             {
-                throw new NotImplementedException();
+                return ElementNode.Root(_provider, name);
             }
 
             public void log(string category, string message)
