ccaaa8bf0b95ff600a1dad2a24bbd6535d712485
Brian Postlethwaite <brian_pos@hotmail.com>
2025-05-05 17:32:52 +1000

    Update FirelySDK 5.11.4

--- demo-map-server/Services/StructureMapService.cs
+++ demo-map-server/Services/StructureMapService.cs
@@ -283,7 +283,7 @@ namespace demo_map_server.Services
 					// Any processing parameters
 					// (including the map that was used to evaluate the request - in StructureMap format)
 					var configParams = new Parameters.ParameterComponent() { Name = "parameters" };
-					configParams.Part.Add(new Parameters.ParameterComponent() { Name = "evaluator", Value = new FhirString(".NET (brianpos) 5.11.3 beta-1") });
+					configParams.Part.Add(new Parameters.ParameterComponent() { Name = "evaluator", Value = new FhirString(".NET (brianpos) 5.11.4 beta-1") });
 					configParams.Part.Add(new Parameters.ParameterComponent() { Name = "map", Resource = sm });
 					result.Parameter.Add(configParams);
 
